91115b3a5219d3969e28939569827d5ac6396df4
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-04-05 21:02:50 +0800

    feat: add passport-facebook & dotevn

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const users = require('./modules/users')
 const todos = require('./modules/todos')
+const auth = require('./modules/auth')
 
 const { authenticator } = require('../middleware/auth')
 
 router.use('/todos', authenticator, todos)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
