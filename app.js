eebb6fc8ac5135a91fa26d2b41e22ef0e6a22b56
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-04-05 20:01:09 +0800

    feat:add authenticator

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
-const passport = require('passport')
 const routes = require('./routes')
+const usePassport = require('./config/passport')
+
 const app = express()
 const PORT = 3000
-const usePassport = require('./config/passport')
-usePassport(app)
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -17,11 +16,17 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
-app.use(routes)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app)
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,10 @@ const home = require('./modules/home')
 const users = require('./modules/users')
 const todos = require('./modules/todos')
 
-router.use('/todos', todos)
+const { authenticator } = require('../middleware/auth')
+
+router.use('/todos', authenticator, todos)
 router.use('/users', users)
-router.use('/', home)
+router.use('/', authenticator, home)
 
 module.exports = router
