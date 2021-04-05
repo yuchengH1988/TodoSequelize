ed2800ecc0e642cc6493bc0222c525d244acc5d2
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-04-05 20:46:13 +0800

    add delete todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -50,4 +50,15 @@ router.put('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+router.delete('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => todo.destroy())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+
 module.exports = router
